a00bbfb7b04f3e0b5658d2dce5d0bc7816c48dd4
Rohan Basnet <rohan.basnet@southeasttech.edu>
2023-11-19 22:03:32 -0600

    Questions 1-4

---
+++ GuitarShop/Models/ProductListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace GuitarShop.Models
+{
+    public class ProductListViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public string Selectedcategory { get ; set; }
+        public string CheckActiveCategory(string category) =>
+            category == Selectedcategory ? "active" : "";
+    }
+}
